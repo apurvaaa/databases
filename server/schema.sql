ff1323ef483a138475235ef2820249c89abcead4
apurvaaa <apurva.shastry@gmail.com>
2017-03-03 17:37:37 -0800

    minor changes in sql file. Out for dinner

--- server/schema.sql
+++ server/schema.sql
@@ -4,11 +4,24 @@ USE chat;
 
 CREATE TABLE messages (
   /* Describe your table here.*/
+  id INT NOT NULL AUTO_INCREMENT,
+  createdAt TIMESTAMP NOT NULL,
+  text VARCHAR(100),
+  roomId INT NOT NULL,
+  userId INT NOT NULL
 );
 
 /* Create other tables and define schemas for them here! */
 
 
+/*tutorials_tbl(
+   tutorial_id INT NOT NULL AUTO_INCREMENT,
+   tutorial_title VARCHAR(100) NOT NULL,
+   tutorial_author VARCHAR(40) NOT NULL,
+   submission_date DATE,
+   PRIMARY KEY ( tutorial_id )
+);
+*/
 
 
 /*  Execute this file from the command line by typing:
